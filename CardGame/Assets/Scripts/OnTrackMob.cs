f83a3982c73fb06d834413ba0e1a0d98cd53773b
Lenxs <48335567+Lenxs@users.noreply.github.com>
2023-02-10 21:54:42 +0100

    vuforia upd

---
+++ CardGame/Assets/Scripts/OnTrackMob.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class OnTrackMob : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void Tracked()
+    {
+        
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
